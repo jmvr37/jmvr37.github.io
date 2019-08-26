bc9a2bb144366a6e5f7259b1fbc588582bb57e92
jose <jmvr37@hotmail.com>
2019-08-25 19:12:02 -0700

    work section fixed

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('.nav-bar ul li a');
+  navLinks.forEach(function(element) {
+    element.addEventListener('click', function(event) {
+      event.preventDefault();
+      console.log('event', event);
+
+      document.querySelector(event.target.hash).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+});
